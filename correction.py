b85ddc1d12a804a6afe80cf855716d3780bc9fc6
CoriMyp <rabcheniuk.m@gmail.com>
2024-02-23 20:18:45 +0200

    14

--- correction.py
+++ correction.py
@@ -4,7 +4,7 @@ import sqlite3 as sql
 db = sql.connect("database.db")
 cursor = db.cursor()
 
-for report in cursor.execute(f"SELECT * FROM reports WHERE partner='AlessandroBets'"):
+for report in cursor.execute(f"SELECT * FROM reports WHERE partner='AlessandroBets'").fetchall():
     cursor.execute(f"UPDATE reports SET id='-1002146867266:{report[0].split(':')[1]}' WHERE id='{report[0]}'")
 
 db.commit()
